123449273ed9072469affe9429cf4eeddac354b7
7sDream <i@7sdre.am>
2023-08-17 01:45:29 +0800

    docs(pairlist): add doc comments

--- pair_list.go
+++ pair_list.go
@@ -34,6 +34,8 @@ func NewPairListFrom[K comparable, V any](list []Pair[K, V]) *PairList[K, V] {
 }
 
 // Get values by key.
+//
+// Performance: O(n)
 func (pl *PairList[K, V]) Get(key K) []V {
 	var values []V
 
@@ -48,6 +50,8 @@ func (pl *PairList[K, V]) Get(key K) []V {
 }
 
 // Has checks if a key exist in the list.
+//
+// Performance: O(n)
 func (pl *PairList[K, V]) Has(key K) bool {
 	for i := range pl.List {
 		if key == pl.List[i].Key {
@@ -59,6 +63,8 @@ func (pl *PairList[K, V]) Has(key K) bool {
 }
 
 // Count get appear times of a key.
+//
+// Performance: O(n)
 func (pl *PairList[K, V]) Count(key K) int {
 	n := 0
 
@@ -73,6 +79,8 @@ func (pl *PairList[K, V]) Count(key K) int {
 
 // GetFirstOrZeroValue get first value by key, return a zero value of type V if
 // key doesn't exist in list.
+//
+// Performance: O(n)
 func (pl *PairList[K, V]) GetFirstOrZeroValue(key K) (value V) {
 	for i := range pl.List {
 		p := &pl.List[i]
@@ -87,6 +95,8 @@ func (pl *PairList[K, V]) GetFirstOrZeroValue(key K) (value V) {
 
 // GetFirstOrZeroValue get last value by key, return a zero value of type V if
 // key doesn't exist in list.
+//
+// Performance: O(n)
 func (pl *PairList[K, V]) GetLastOrZeroValue(key K) (value V) {
 	for i := pl.Len() - 1; i >= 0; i-- {
 		p := &pl.List[i]
@@ -99,44 +109,66 @@ func (pl *PairList[K, V]) GetLastOrZeroValue(key K) (value V) {
 	return
 }
 
+// GetKeyByIndex get key at index.
+//
+// You should make sure 0 <= i < Len(), panic if out of bound.
 func (pl *PairList[K, V]) GetKeyByIndex(index int) K {
 	return pl.List[index].Key
 }
 
+// GetByIndex get key value pair at index.
+//
+// You should make sure 0 <= i < Len(), panic if out of bound.
 func (pl *PairList[K, V]) GetByIndex(index int) Pair[K, V] {
 	return pl.List[index]
 }
 
+// GetValueByIndex get value at index.
+//
+// You should make sure 0 <= i < Len(), panic if out of bound.
 func (pl *PairList[K, V]) GetValueByIndex(index int) V {
 	return pl.List[index].Value
 }
 
+// Add a key value pair to the end of list.
 func (pl *PairList[K, V]) Add(key K, value V) {
 	pl.List = append(pl.List, Pair[K, V]{key, value})
 }
 
+// Append some key value pairs to the end of list.
 func (pl *PairList[K, V]) Append(pairs ...Pair[K, V]) {
 	pl.List = append(pl.List, pairs...)
 }
 
+// Delete all item whose key is same as provided.
+//
+// Performance: O(n)
 func (pl *PairList[K, V]) Delete(key K) {
 	pl.Filter(func(p *Pair[K, V]) bool {
 		return p.Key != key
 	})
 }
 
+// DeleteByIndex delete item at index.
+//
+// Performance: O(n)
 func (pl *PairList[K, V]) DeleteByIndex(index int) {
 	pl.List = append(pl.List[:index], pl.List[index+1:]...)
 }
 
+// Clean this list.
 func (pl *PairList[K, V]) Clear() {
 	pl.List = nil
 }
 
+// Len returns the size of list.
 func (pl *PairList[K, V]) Len() int {
 	return len(pl.List)
 }
 
+// Keys returns all keys of the list.
+//
+// Performance: O(n).
 func (pl *PairList[K, V]) Keys() []K {
 	keys := make([]K, 0, pl.Len())
 	for i := 0; i < pl.Len(); i++ {
@@ -145,6 +177,9 @@ func (pl *PairList[K, V]) Keys() []K {
 	return keys
 }
 
+// Values returns all values of the list.
+//
+// Performance: O(n).
 func (pl *PairList[K, V]) Values() []V {
 	values := make([]V, 0, pl.Len())
 	for i := 0; i < pl.Len(); i++ {
@@ -153,6 +188,7 @@ func (pl *PairList[K, V]) Values() []V {
 	return values
 }
 
+// ToMap convert this list into a [Map], with provided [DuplicatedKeyStrategy].
 func (pl *PairList[K, V]) ToMap(strategy DuplicatedKeyStrategy) *Map[K, V] {
 	m := NewMap[K, V]()
 	m.SetDuplicatedKeyStrategy(strategy)
@@ -160,10 +196,14 @@ func (pl *PairList[K, V]) ToMap(strategy DuplicatedKeyStrategy) *Map[K, V] {
 	return m
 }
 
+// Dedup deduplicates this list by key.
+//
+// Implemented as converting it to a [Map] and back.
 func (pl *PairList[K, V]) Dedup(strategy DuplicatedKeyStrategy) {
 	pl.List = pl.ToMap(strategy).Pairs().List
 }
 
+// Sort will reorder the list using the given less function.
 func (pl *PairList[K, V]) Sort(lessFunc PairLessFunc[K, V]) {
 	sort.SliceStable(pl.List, func(i, j int) bool {
 		return lessFunc(&pl.List[i], &pl.List[j])
@@ -172,7 +212,8 @@ func (pl *PairList[K, V]) Sort(lessFunc PairLessFunc[K, V]) {
 
 // Filter remove all item which make pred func return false.
 //
-// More efficient then `GetByIndex` + `DeleteByIndex` in a loop.
+// Performance: O(n). More efficient then [PairList.GetByIndex] +
+// [PairList.DeleteByIndex] in a loop, which is O(n^2).
 func (pl *PairList[K, V]) Filter(pred PairFilterFunc[K, V]) {
 	n := 0
 	for i, length := 0, pl.Len(); i < length; i++ {
